f8dfabc170754efd4e581fe013a25b6e61240f67
oomusou <oomusou@gmail.com>
2016-04-20 21:22:10 +0800

    安裝 Test Factory Generator

--- config/app.php
+++ config/app.php
@@ -148,6 +148,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
+        Mpociot\LaravelTestFactoryHelper\TestFactoryHelperServiceProvider::class,
 
         /*
          * Application Service Providers...
